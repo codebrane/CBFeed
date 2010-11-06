3c9bde28a4155b070dfd0288bc28cd0e79e270c4
Alistair Young <alistair@codebrane.com>
2010-11-06 17:28:20 +0000

    Added feed description

--- CBFeed.h
+++ CBFeed.h
@@ -4,9 +4,11 @@
 @interface CBFeed : NSObject {
   NSString *title;
   NSString *link;
+  NSString *description;
 }
 
 @property (nonatomic, retain) NSString *title;
 @property (nonatomic, retain) NSString *link;
+@property (nonatomic, retain) NSString *description;
 
 @end
